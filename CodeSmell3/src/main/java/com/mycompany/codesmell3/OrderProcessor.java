754e66642c11b6ce45dbe84e6b54299e0440277c
bimo.adi.setiawan <bimoas@it.student.pens.ac.id>
2024-11-07 20:17:40 +0700

    smell-code-3

--- CodeSmell3/src/main/java/com/mycompany/codesmell3/OrderProcessor.java
+++ CodeSmell3/src/main/java/com/mycompany/codesmell3/OrderProcessor.java
@@ -9,9 +9,17 @@ package com.mycompany.codesmell3;
  * @author andhik
  */
 public class OrderProcessor {
-    public void processOrder(String customerName, String address, String email, 
-            String product, int quantity, double price) {
+     public void processOrder(Order order) {
+        // Logika pemrosesan pesanan menggunakan data dari objek Order
+        String customerName = order.getCustomerName();
+        String address = order.getAddress();
+        String email = order.getEmail();
+        String product = order.getProduct();
+        int quantity = order.getQuantity();
+        double price = order.getPrice();
+
         // Logika pemrosesan pesanan
+        System.out.println("Processing order for: " + customerName);
     }
 }
 
