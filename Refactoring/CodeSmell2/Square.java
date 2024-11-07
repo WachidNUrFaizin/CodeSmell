72141d4036a5c2590bb5dccd451ac010ee52963d
Faizin03 <119776413+Faizin03@users.noreply.github.com>
2024-11-07 20:22:11 +0700

    2. Primitive Obsession

---
+++ Refactoring/CodeSmell2/Square.java
@@ -0,0 +1,11 @@
+public class Square {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double calculateArea() {
+        return side * side;
+    }
+}
